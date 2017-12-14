6817b1ee66ddaff9cb088a4661a844ef0a075f17
Philipp Paulweber <p.paulweber@gmail.com>
2017-12-14 12:39:38 +0100

    CI
    * fixed script path by adding proper '.sh' file extension
    * updated license text

--- .ci/script/test-run.sh
+++ .ci/script/test-run.sh
@@ -1,26 +1,26 @@
-#!/bin/bash
-#   
-#   Copyright (c) 2014-2016 CASM Organization
+#
+#   Copyright (c) 2014-2017 CASM Organization https://casm-lang.org
 #   All rights reserved.
-#   
+#
 #   Developed by: Philipp Paulweber
+#                 Emmanuel Pescosta
 #                 https://github.com/casm-lang/casm
-#   
+#
 #   This file is part of casm.
-#   
+#
 #   casm is free software: you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by
 #   the Free Software Foundation, either version 3 of the License, or
 #   (at your option) any later version.
-#   
+#
 #   casm is distributed in the hope that it will be useful,
 #   but WITHOUT ANY WARRANTY; without even the implied warranty of
 #   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 #   GNU General Public License for more details.
-#   
+#
 #   You should have received a copy of the GNU General Public License
 #   along with casm. If not, see <http://www.gnu.org/licenses/>.
-#   
+#
 
 if [ -z "$REPO" ]; then
     echo "error: environment variable 'REPO' is not set!"
@@ -33,12 +33,9 @@ OUT=out
 set -e -x
 
 date
+
 uname -a
 
-cnt=0
-for i in $LIBS; do
-    cp -rf lib$cnt $OUT/$i
-    cnt=$((cnt + 1))
-done
+make -C $IN/$REPO test-run
 
-cp -rf $IN/repo $OUT/$REPO
+cp -rf $IN/* $OUT/
