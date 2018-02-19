3646a17caee2366d1529f409acfe8fb16db89b81
Philipp Paulweber <p.paulweber@gmail.com>
2018-02-19 01:08:06 +0100

    CI
    * added mail script and task configuration

---
+++ .ci/script/mail.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#
+#   Copyright (C) 2014-2018 CASM Organization <https://casm-lang.org>
+#   All rights reserved.
+#
+#   Developed by: Philipp Paulweber
+#                 Emmanuel Pescosta
+#                 <https://github.com/casm-lang/casm>
+#
+#   This file is part of casm.
+#
+#   casm is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   casm is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with casm. If not, see <http://www.gnu.org/licenses/>.
+#
+
+set -e
+
+date
+
+uname -a
+
+git log -1 --pretty=format:'"%an" <%ae>' > mail_to
+
+echo "$BUILD_TEAM_NAME/$BUILD_PIPELINE_NAME/$BUILD_JOB_NAME/$BUILD_NAME ($BUILD_ID)" > mail_subject
+
+echo "$ATC_EXTERNAL_URL/teams/$BUILD_TEAM_NAME/pipelines/$BUILD_PIPELINE_NAME/jobs/$BUILD_JOB_NAME/builds/$BUILD_NAME ($BUILD_ID)" > mail_body
