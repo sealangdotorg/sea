c035f73b2780cabe9c2b3e0ea32e1e7e6d1bf544
Philipp Paulweber <p.paulweber@gmail.com>
2021-01-02 16:07:03 +0100

    Happy New Year :tada:

--- .ci/script/pipeline.sh
+++ .ci/script/pipeline.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-#   Copyright (C) 2014-2020 CASM Organization <https://casm-lang.org>
+#   Copyright (C) 2014-2021 CASM Organization <https://casm-lang.org>
 #   All rights reserved.
 #
 #   Developed by: Philipp Paulweber
